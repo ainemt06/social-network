6717dbcbb085b7844b655fb46000197d30f7c720
ainemt06 <ainemt06@gmail.com>
2024-10-10 19:37:36 -0400

    created a helper class to hold onto individual edges, and established network constructor

---
+++ src/network/Connection.java
@@ -0,0 +1,26 @@
+package network;
+
+import java.util.Objects;
+
+class Connection<A, B> {
+
+    private final A first;
+    private final B second;
+
+    public Connection(A thing1, B thing2) {
+        Objects.requireNonNull(thing1);
+        Objects.requireNonNull(thing2);
+
+        first = thing1;
+        second = thing2;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+}
---
+++ src/network/Network.java
@@ -0,0 +1,16 @@
+package network;
+
+import person.Person;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Network {
+    private final SortedSet<Connection<Person, Person>> edges;
+    private final SortedSet<Person> nodes;
+
+    public Network() {
+        edges = new TreeSet<Connection<Person, Person>>();
+        nodes = new TreeSet<Person>();
+    }
+}
